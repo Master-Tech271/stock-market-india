63aeea6dcfe6b7df6553d8973452975175b7dd23
Master-Tech271 <umar17605@gmail.com>
2021-05-21 01:07:53 +0530

    comment port no

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var app = express();
 var cors = require("cors");
 app.use(cors());
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server running on port 3000");
+// });
 app.use(cors());
 app.use
 // National Stock Exchange (NSE) APIS
